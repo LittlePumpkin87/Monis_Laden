6148d643a8ba497b90b43c8d42d5ebbe323fb48d
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2024-03-06 19:37:11 +0100

    update march

---
+++ js/main.js
@@ -0,0 +1,106 @@
+//////////////////////////////----------------------------GALLERY----------------------------//////////////////////////////
+
+
+$('.slider').each(function () {
+  let $this = $(this);
+  let $group = $this.find('.slide_group');
+  let $slides = $this.find('.slide');
+  let bulletArray = [];
+  let currentIndex = 0;
+  let timeout;
+
+  function move(newIndex) {
+    let animateLeft, slideLeft;
+
+    advance();
+
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function () {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+
+  $('.next_btn').on('click', function () {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+
+  $('.previous_btn').on('click', function () {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(7);
+    }
+  });
+
+  $.each($slides, function (index) {
+    let $button = $('<a class="slide_btn">&bull;</a>');
+
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function () {
+      move(index);
+    }).appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+
+  advance();
+});
+
+//////////////////////////////----------------------------Hide Navbar when scroll----------------------------//////////////////////////////
+
+let prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-300px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
